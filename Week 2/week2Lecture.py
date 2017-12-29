0d81bdbef1af438f79bdecf9737f908f29ec01f7
Benson Lau <bensonjaclyn.lau@gmail.com>
2017-12-28 23:34:01 -0800

    Adding in additional example

--- Week 2/week2Lecture.py
+++ Week 2/week2Lecture.py
@@ -4,6 +4,7 @@ import numpy as np
 # Inverted Matrix Solution
 ###############################
 
+#####Example 1#####
 #2x - 3y = 6
 #x + y = 0
 
@@ -24,4 +25,23 @@ results = np.dot(inv_var_matrix, objective)
 
 # Results are the value for x and y that meets the objective 
 print(results)
-#[[1.2,-1.2]]
+#[[ 1.2 -1.2]]
+
+#####Example 2#####
+#x + 2y = 4
+#3x - 5y = 1
+
+eq_1 = [1, 2]
+eq_2 = [3, -5]
+
+objective = [4, 1]
+
+var_matrix = np.matrix([eq_1,
+                        eq_2])
+                        
+inv_var_matrix = np.linalg.inv(var_matrix)
+
+results = np.dot(inv_var_matrix, objective)
+
+print(results)
+#[[ 2.  1.]]
